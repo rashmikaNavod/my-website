faebabc2960943794729a12259d9a8dde95aaf36
rashmikaNavod <drashmikanavod@gmail.com>
2024-09-27 20:54:49 +0530

    add header style

--- Scripts/script.js
+++ Scripts/script.js
@@ -1,4 +1,6 @@
 const sideMenu = document.querySelector('#sideMenu');
+const headerBar = document.querySelector('header')
+const headerLinks = document.querySelector('header ul')
 
 function openMenu(){
     sideMenu.style.transform = 'translateX(-100vw)';
@@ -6,3 +8,13 @@ function openMenu(){
 function closeMenu(){
     sideMenu.style.transform = 'translateX(100vw)';
 }
+
+window.addEventListener('scroll', ()=>{
+    if(scrollY > 50){
+        headerBar.classList.add('bg-white');
+        headerLinks.classList.remove('nav-list2');
+    }else{
+        headerBar.classList.remove('bg-white');
+        headerLinks.classList.add('nav-list2');
+    }
+})
