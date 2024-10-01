0c515868217523a69759f0ee8a04599517aafdac
rashmikaNavod <drashmikanavod@gmail.com>
2024-10-01 22:44:48 +0530

    Add loading animation

--- Scripts/script.js
+++ Scripts/script.js
@@ -1,3 +1,9 @@
+var loader = document.getElementById("preloader");
+window.addEventListener("load" , function(){
+    loader.style.display = "none";
+})
+
+
 const sideMenu = document.querySelector('#sideMenu');
 const headerBar = document.querySelector('header')
 const headerLinks = document.querySelector('header ul')
